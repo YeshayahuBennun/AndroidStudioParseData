3e2909996d6ec4cc259af5025f539ae2e869bfc1
YeshayahuBennun <ybennun@yahoo.com>
2022-01-14 14:19:42 +0200

    commit#3 - Parse Data -  Set Up JSONArray Android Volley

--- app/src/main/java/com/ybennun/parsedata/MainActivity.java
+++ app/src/main/java/com/ybennun/parsedata/MainActivity.java
@@ -9,20 +9,35 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONObject;
 
+public class MainActivity extends AppCompatActivity {
+    RequestQueue queue;
     String url = "https://www.google.com";
+    String apiUrl = "https://jsonplaceholder.typicode.com/todos";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RequestQueue queue = Volley.newRequestQueue(this);
+        queue = Volley.newRequestQueue(this);
 
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, apiUrl, null,
+                response -> Log.d("JSON", "onCreate: " + response.toString()),
+                error -> Log.d("JSON", "onCreate: Failed! "));
+
+        queue.add(jsonArrayRequest);
+
+        getString(queue);
+    }
+
+    private void getString(RequestQueue queue) {
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, response -> {
             //display the contents of our url
             Log.d("Main", "onCreate:" + response.substring(0, 500));
